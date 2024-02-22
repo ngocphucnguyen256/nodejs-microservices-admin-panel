13d4dd5f19102a5e8564d310b3e702772df4a060
Nguyễn Ngọc Phúc <ngocphuc.nguyen256@gmail.com>
2024-02-22 14:49:28 +0700

    connect db, fix hot reload

--- users-service/src/index.ts
+++ users-service/src/index.ts
@@ -1,1 +1,14 @@
+import "reflect-metadata";
+import { initConnection } from "#root/db/connection";
+require('dotenv').config()
+
 console.log("🙋‍♂️ Hello from Job User Service !!!!!!!");
+
+initConnection().then(()=>{
+    console.log("🔗 Connected to DB");
+
+}).catch(err => {
+  console.error(err);
+});
+
+
